71582034f3f5ae46897cb2bebbd25c10d85f37ad
mohammod-milon <112416603+mohammod-milon@users.noreply.github.com>
2024-11-07 12:53:10 +0600

    Create app.py
    Hacker is back

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''
+    <form action="/login" method="post">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password"><br>
+        <input type="submit" value="Login">
+    </form>
+    '''
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    return f'Username: {username}, Password: {password}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
